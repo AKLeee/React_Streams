789a879fb07ccb8918801bb7031afbcb64607c3d
AKLeee <angkoulee@gmail.com>
2019-03-22 15:30:25 -0700

    initial setup and create files for router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+// BrowserRouter will look up the router, if not found, not return 404, but index.html, that how create-react-app works
+// HashRouter will always return index.html and ignore the router after # tag
+
+// const PageOne = () => {
+//   return <div>PageOne
+//     {/* BAD!! as acher tag will request server and load index.html again and reload all app, old data/page is dumped
+//     <a href="/pagetwo">Navigate to page two</a> */}
+//     <Link to="/pagetwo">Navigate to page two</Link>
+//   </div>
+// };
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
